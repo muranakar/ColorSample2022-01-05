f72d741d67c1f48408b2008f980136004a802986
ryoryoryoooo <P1211065@gmail.com>
2022-01-05 10:07:00 +0900

    navigation design

--- ColorSample2022-01-05/ViewController.swift
+++ ColorSample2022-01-05/ViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var viewTop: UIView!
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.backgroundColor = Colors.complementary1Color
+        viewTop.backgroundColor = Colors.complementary2Color
+
+        var view11 = UIView()
+
+
+
     }
+
 
 
 }
